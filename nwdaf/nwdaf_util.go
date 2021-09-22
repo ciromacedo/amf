275310e0654da6c86c67090e895dc2bf46ed9eab
ciromacedo <ciromacedo@gmail.com>
2021-09-22 13:57:47 -0300

    add new features

--- nwdaf/nwdaf_util.go
+++ nwdaf/nwdaf_util.go
@@ -3,10 +3,8 @@ package nwdaf
 import (
 	"bytes"
 	"crypto/tls"
-	"fmt"
 	"github.com/free5gc/amf/context"
 	"golang.org/x/net/http2"
-	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
@@ -14,13 +12,11 @@ import (
 
 
 const NwdafUrl = "http://127.0.0.1:29599";
-const RegistrationAcceptApi = "/amf-contexts/registration-accept";
-const DateFormat = "02/Jan/2006 15:04:05";
+const RegistrationAcceptApi = "/datacollection/amf-contexts/registration-accept";
+
 
 func RegistrationAccept(amfUe *context.AmfUe){
-	url := getUrlCoonection(RegistrationAcceptApi)
-
-	fmt.Println(amfUe.ServingAMF().NfId)
+	url := getUrlConnection(RegistrationAcceptApi)
 
 	jsonData := `
     {	
@@ -33,7 +29,6 @@ func RegistrationAccept(amfUe *context.AmfUe){
 	}
 	`
 
-	//criando o JSOn
 	var jsonStr = []byte(jsonData)
 	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
 	req.Header.Set("X-Custom-Header", "myvalue")
@@ -47,54 +42,21 @@ func RegistrationAccept(amfUe *context.AmfUe){
 	}
 
 	defer resp.Body.Close()
-
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("response Body:", string(body))
 }
 
-func getUrlCoonection(str string)(string){
+func getUrlConnection(str string)(string){
 	return NwdafUrl +str;
 }
-
-
 
 
 func getConnection()(http.Client){
 	client := http.Client{
 		Transport: &http2.Transport{
-			// So http2.Transport doesn't complain the URL scheme isn't 'https'
 			AllowHTTP: true,
-			// Pretend we are dialing a TLS endpoint. (Note, we ignore the passed tls.Config)
 			DialTLS: func(network, addr string, cfg *tls.Config) (net.Conn, error) {
 				return net.Dial(network, addr)
 			},
 		},
 	}
 	return client
-}
-
-func NWDAFConnection() {
-	url := NwdafUrl +"/amf-contexts/registration-accept";
-
-	//criando o JSOn
-	var jsonStr = []byte(`{"title":"Buy cheese and bread for breakfast."}`)
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
-	req.Header.Set("X-Custom-Header", "myvalue")
-	req.Header.Set("Content-Type", "application/json")
-
-	client := getConnection()
-
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer resp.Body.Close()
-
-	fmt.Println("response Status:", resp.Status)
-	fmt.Println("response Headers:", resp.Header)
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println("response Body:", string(body))
 }
